8a466b8a264118305974eb0bcd4e694f86ec0c0e
ozodbekqosimov <o0319353@gmail.com>
2025-05-13 19:34:35 +0500

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,40 +1,40 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction } from '@reduxjs/toolkit'
 import {
-  filtersFetched,
-  filtersFetching,
-  filtersFetchingError,
-} from "../slice/filters-slice";
+	filtersFetched,
+	filtersFetching,
+	filtersFetchingError,
+} from '../slice/filters-slice'
 import {
-  playersFetched,
-  playersFetching,
-  playersFetchingError,
-} from "../slice/players-slice";
+	playersFetched,
+	playersFetching,
+	playersFetchingError,
+} from '../slice/players-slice'
 
-// export const fetchPlayer = (request) => (dispatch) => {
-//   dispatch(playersFetching());
+export const fetchPlayer = request => dispatch => {
+	dispatch(playersFetching())
 
-//   request("http://localhost:3000/players")
-//     .then((data) => {
-//       dispatch(playersFetched(data));
-//     })
-//     .catch(() => dispatch(playersFetchingError()));
-// };
+	request('http://localhost:3000/players')
+		.then(data => {
+			dispatch(playersFetched(data))
+		})
+		.catch(() => dispatch(playersFetchingError()))
+}
 
-export const fetchFilters = (request) => (dispatch) => {
-  dispatch(filtersFetching());
+export const fetchFilters = request => dispatch => {
+	dispatch(filtersFetching())
 
-  request("http://localhost:3000/filters")
-    .then((data) => dispatch(filtersFetched(data)))
-    .catch(() => dispatch(filtersFetchingError()));
-};
+	request('http://localhost:3000/filters')
+		.then(data => dispatch(filtersFetched(data)))
+		.catch(() => dispatch(filtersFetchingError()))
+}
 
-// export const playersFetching = createAction("PLAYERS_FETCHING");
-// export const playersFetched = createAction("PLAYERS_FETCHED");
-// export const playersFetchingError = createAction("PLAYERS_FETCHING_ERROR");
-// export const createPlayer = createAction("PLAYER_CREATED");
-// export const deletePlayer = createAction("PLAYER_DELETED");
+export const playersFetching = createAction('PLAYERS_FETCHING')
+export const playersFetched = createAction('PLAYERS_FETCHED')
+export const playersFetchingError = createAction('PLAYERS_FETCHING_ERROR')
+export const createPlayer = createAction('PLAYER_CREATED')
+export const deletePlayer = createAction('PLAYER_DELETED')
 
-// export const filtersFetching = createAction("FILTERS_FETCHING");
-// export const filtersFetched = createAction("FILTERS_FETCHED");
-// export const filtersFetchingError = createAction("FILTERS_FETCHING_ERROR");
-// export const activeFilterChanged = createAction("PLAYERS_FILTERED");
+export const filtersFetching = createAction('FILTERS_FETCHING')
+export const filtersFetched = createAction('FILTERS_FETCHED')
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR')
+export const activeFilterChanged = createAction('PLAYERS_FILTERED')
--- src/components/players-list.jsx
+++ src/components/players-list.jsx
@@ -1,67 +1,71 @@
-import { useCallback } from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { createSelector } from "@reduxjs/toolkit";
-import { deletePlayer, fetchPlayers } from "../slice/players-slice";
-import useHttp from "../hooks/use-http";
-import Empty from "./empty";
-import Error from "./error";
-import PlayersListItem from "./players-list-item";
-import Spinner from "./spinner";
+import { useCallback } from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { createSelector } from '@reduxjs/toolkit'
+import { deletePlayer, fetchPlayers } from '../slice/players-slice'
+import useHttp from '../hooks/use-http'
+import Empty from './empty'
+import Error from './error'
+import PlayersListItem from './players-list-item'
+import Spinner from './spinner'
 
 const PlayersList = () => {
-  //
-  const filteredPlayersSelector = createSelector(
-    (state) => state.filters.activeFilter,
-    (state) => state.players.players,
-    (filter, players) => {
-      if (filter === "All") {
-        return players;
-      } else {
-        return players.filter((player) => player.continent === filter);
-      }
-    }
-  );
+	//
+	const filteredPlayersSelector = createSelector(
+		state => state.filters.activeFilter,
+		state => state.players.players,
+		(filter, players) => {
+			if (filter === 'All') {
+				return players
+			} else {
+				return players.filter(player => player.continent === filter)
+			}
+		}
+	)
 
-  const filteredPlayers = useSelector(filteredPlayersSelector);
+	const filteredPlayers = useSelector(filteredPlayersSelector)
 
-  const playerLoadingStatus = useSelector(
-    (state) => state.players.playerLoadingStatus
-  );
+	const playerLoadingStatus = useSelector(
+		state => state.players.playerLoadingStatus
+	)
 
-  const { request } = useHttp();
-  const dispatch = useDispatch();
+	const { request } = useHttp()
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(fetchPlayers());
-  }, []);
+	useEffect(() => {
+		dispatch(fetchPlayers())
+	}, [])
 
-  const onDelete = useCallback(
-    (id) => {
-      request(`http://localhost:3000/players/${id}`, "DELETE")
-        .then((res) => console.log(res, "Deleted successfully"))
-        .then(dispatch(deletePlayer(id)))
-        .catch((e) => console.log(e));
-    },
-    [request]
-  );
+	const onDelete = useCallback(
+		id => {
+			request(`http://localhost:3000/players/${id}`, 'DELETE')
+				.then(res => console.log(res, 'Deleted successfully'))
+				.then(dispatch(deletePlayer(id)))
+				.catch(e => console.log(e))
+		},
+		[request]
+	)
 
-  if (playerLoadingStatus === "loading") {
-    return <Spinner classNames={"text-white w-8 h-8 mx-auto"} />;
-  } else if (playerLoadingStatus === "error") {
-    return <Error />;
-  }
+	if (playerLoadingStatus === 'loading') {
+		return <Spinner classNames={'text-white w-8 h-8 mx-auto'} />
+	} else if (playerLoadingStatus === 'error') {
+		return <Error />
+	}
 
-  const renderPlayers = () => {
-    if (filteredPlayers.length === 0) {
-      return <Empty />;
-    }
-    return filteredPlayers.map(({ id, ...props }) => (
-      <PlayersListItem key={id} {...props} onDelete={() => onDelete(id)} />
-    ));
-  };
+	const renderPlayers = () => {
+		if (filteredPlayers.length === 0) {
+			return <Empty />
+		}
+		return filteredPlayers.map(({ id, ...props }) => (
+			<PlayersListItem key={id} {...props} onDelete={() => onDelete(id)} />
+		))
+	}
 
-  return <div className="space-y-5">{renderPlayers()}</div>;
-};
+	return (
+		<div className='space-y-5 overflow-scroll h-[80%] p-4'>
+			{renderPlayers()}
+		</div>
+	)
+}
 
-export default PlayersList;
+export default PlayersList
